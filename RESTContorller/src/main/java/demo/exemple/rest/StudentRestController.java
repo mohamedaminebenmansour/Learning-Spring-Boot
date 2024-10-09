7db01075db4d63309309ba02ac9f8994fc6de212
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-10-09 22:33:42 +0100

    @PostConstruct to loadData just one for learning propose

--- RESTContorller/src/main/java/demo/exemple/rest/StudentRestController.java
+++ RESTContorller/src/main/java/demo/exemple/rest/StudentRestController.java
@@ -8,18 +8,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import demo.exemple.entity.Student;
+import jakarta.annotation.PostConstruct;
 
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
 
 	//define endpoint for "/students" return a list of students
-	@GetMapping("/students")
-	public List<Student> getStudents(){
-		List <Student> theStudents=new ArrayList<>();
+	List <Student> theStudents;
+	
+	@PostConstruct
+	public void loadData() {
+		theStudents=new ArrayList<>();
 		theStudents.add(new Student("Mohamed","Benmasnour"));
 		theStudents.add(new Student("Mouna","wahada"));
 		theStudents.add(new Student("damine","haj"));
+		
+	}
+	@GetMapping("/students")
+	public List<Student> getStudents(){
+		
 		return theStudents;
 	}
 }
