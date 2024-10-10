bf21b4619fdf637ab964892611f666a00d3be2f0
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-10-10 10:03:17 +0100

    Exception Handling

--- RESTContorller/src/main/java/demo/exemple/rest/StudentRestController.java
+++ RESTContorller/src/main/java/demo/exemple/rest/StudentRestController.java
@@ -55,4 +55,17 @@ public class StudentRestController {
 		//return RespnseEntity
 		return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
 	}
+	
+	//add another execption handler ... to catch any exception(catch all)
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
+		//create a StudentErrorResponse
+		StudentErrorResponse error = new StudentErrorResponse();
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		//return RespnseEntity
+		return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
+			
+	}
 }
