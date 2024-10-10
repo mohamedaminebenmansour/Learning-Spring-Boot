c97288eabe0db217d97c5999b08e3020e0ecf4af
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-10-10 10:27:01 +0100

    Create entity

---
+++ CRUD_demo/src/main/java/demo/exemple/entity/Employee.java
@@ -0,0 +1,33 @@
+package demo.exemple.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name="employee")
+public class Employee {
+
+	//define fields
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	private String email;
+}
