87bd55088e6e15a02ab40d596c235d4c2c5d1f46
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-10-10 10:11:22 +0100

    Refactor REST service ... remove exception handling code

--- RESTContorller/src/main/java/demo/exemple/rest/StudentRestExceptionHandler.java
+++ RESTContorller/src/main/java/demo/exemple/rest/StudentRestExceptionHandler.java
@@ -1,5 +1,38 @@
 package demo.exemple.rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class StudentRestExceptionHandler {
 
+	//add exception handling code here
+	//Add an exception handler using 
+		@ExceptionHandler
+		public ResponseEntity<StudentErrorResponse> handlerException(StudentNotFoundException exc){
+			
+			//create a StudentErrorResponse
+			StudentErrorResponse error = new StudentErrorResponse();
+			error.setStatus(HttpStatus.NOT_FOUND.value());
+			error.setMessage(exc.getMessage());
+			error.setTimeStamp(System.currentTimeMillis());
+			//return RespnseEntity
+			return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+		}
+		
+		//add another execption handler ... to catch any exception(catch all)
+		@ExceptionHandler
+		public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
+			//create a StudentErrorResponse
+			StudentErrorResponse error = new StudentErrorResponse();
+			error.setStatus(HttpStatus.BAD_REQUEST.value());
+			error.setMessage(exc.getMessage());
+			error.setTimeStamp(System.currentTimeMillis());
+			//return RespnseEntity
+			return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
+				
+		}
+	
 }
