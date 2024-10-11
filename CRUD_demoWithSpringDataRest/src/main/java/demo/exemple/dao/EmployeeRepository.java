ef2f8231f2656bb713c6755597802e93437c798c
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-10-11 10:27:19 +0100

    default from 20 to 3

--- CRUD_demoWithSpringDataRest/src/main/java/demo/exemple/dao/EmployeeRepository.java
+++ CRUD_demoWithSpringDataRest/src/main/java/demo/exemple/dao/EmployeeRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import demo.exemple.entity.Employee;
 
-@RepositoryRestResource(path="members")
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	//that's it ...
