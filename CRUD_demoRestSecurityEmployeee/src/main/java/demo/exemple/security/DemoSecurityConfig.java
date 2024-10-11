82ec1d60d501ba6d281e225394ad22bca8341214
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-10-11 11:42:41 +0100

    Restrict URLs based on Roles

--- CRUD_demoRestSecurityEmployeee/src/main/java/demo/exemple/security/DemoSecurityConfig.java
+++ CRUD_demoRestSecurityEmployeee/src/main/java/demo/exemple/security/DemoSecurityConfig.java
@@ -2,9 +2,12 @@ package demo.exemple.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -28,5 +31,24 @@ public class DemoSecurityConfig {
 				.roles("EMPOLUEE","MANAGER","ADMIN")
 				.build();
 		return new InMemoryUserDetailsManager(tabbabi,wahada,benmansour);
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain (HttpSecurity http) throws Exception{
+		http.authorizeHttpRequests(configurer->
+				configurer
+				.requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
+				.requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
+				.requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
+				.requestMatchers(HttpMethod.PUT,"/api/employees").hasRole("MANAGER")
+				.requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
+		);
+		//use HTTP Basic authentication
+		http.httpBasic();
+		//disable Corss Site Request Forgery
+		//In general, not required for statless REST APIs that use POST, PUT, DELETE and/or PATCH
+
+		http.csrf().disable();
+		return http.build();
 	}
 }
