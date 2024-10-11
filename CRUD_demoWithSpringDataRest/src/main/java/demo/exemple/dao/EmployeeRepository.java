28f54a7a8bb8af0a7ec2ac750305d571dd02569e
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-10-11 10:24:30 +0100

    change endpoint from employees to members

--- CRUD_demoWithSpringDataRest/src/main/java/demo/exemple/dao/EmployeeRepository.java
+++ CRUD_demoWithSpringDataRest/src/main/java/demo/exemple/dao/EmployeeRepository.java
@@ -2,9 +2,11 @@
 package demo.exemple.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import demo.exemple.entity.Employee;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	//that's it ...
