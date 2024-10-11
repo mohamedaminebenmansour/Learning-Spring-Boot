ef638f13a23d6839cd4c72dd99660d95357c225b
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-10-11 11:13:21 +0100

    Add users, passwords and roles

--- CRUD_demoRestSecurityEmployeee/src/main/java/demo/exemple/security/DemoSecurityConfig.java
+++ CRUD_demoRestSecurityEmployeee/src/main/java/demo/exemple/security/DemoSecurityConfig.java
@@ -1,8 +1,32 @@
 package demo.exemple.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class DemoSecurityConfig {
 
+	
+	@Bean
+	public InMemoryUserDetailsManager userDetailsManager() {
+		UserDetails tabbabi = User.builder()
+				.username("mohamed")
+				.password("{noop}test123")
+				.roles("EMPLOYEE")
+				.build();
+		UserDetails wahada = User.builder()
+				.username("mouna")
+				.password("{noop}test123")
+				.roles("EMPOLUEE","MANAGER")
+				.build();
+		UserDetails benmansour = User.builder()
+				.username("damin")
+				.password("{noop}test123")
+				.roles("EMPOLUEE","MANAGER","ADMIN")
+				.build();
+		return new InMemoryUserDetailsManager(tabbabi,wahada,benmansour);
+	}
 }
